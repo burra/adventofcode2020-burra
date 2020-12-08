a40d22928af61f04b39b1add65c5af9376c44a1e
Bertil Bäck <bertil.back@gmail.com>
2020-12-08 21:34:53 +0200

    added part 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,23 +10,33 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         try {
-           ArrayList<String> lines = new ArrayList<>(Files.readAllLines(Paths.get("data/input1.txt")));
+            ArrayList<String> lines = new ArrayList<>(Files.readAllLines(Paths.get("data/input1.txt")));
 
-           for (int i = 0;  i < lines.size(); i++) {
-               for (int j = 0;  j < lines.size(); j++) {
+            for (int i = 0; i < lines.size(); i++) {
+                for (int j = 0; j < lines.size(); j++) {
                     if (Integer.parseInt(lines.get(i)) + Integer.parseInt(lines.get(j)) == 2020) {
-                        System.out.println("Match for 2020");
+                        System.out.println("Match for part 1 2020");
                         System.out.println(lines.get(i));
                         System.out.println(lines.get(j));
                         System.out.println(Integer.parseInt(lines.get(i)) * Integer.parseInt(lines.get(j)));
                     }
-               }
-           }
-        }
+                        for (int k = 0; k < lines.size(); k++) {
+                            if (Integer.parseInt(lines.get(i)) + Integer.parseInt(lines.get(j)) +
+                                    Integer.parseInt(lines.get(k)) == 2020) {
+                                    System.out.println("Match for part 2 for 2020");
+                                    System.out.println(lines.get(i));
+                                    System.out.println(lines.get(j));
+                                    System.out.println(lines.get(k));
+                                    System.out.println(Integer.parseInt(lines.get(i)) * Integer.parseInt(lines.get(j)) * Integer.parseInt(lines.get(k)));
+
+                           }
+                        }
+                    }
+                }
+            }
         catch (IOException e) {
             System.out.println("file open error");
             // Handle a potential exception
-
        }
     }
 }
